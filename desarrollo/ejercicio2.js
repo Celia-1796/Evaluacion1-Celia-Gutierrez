b0961636c68fde71f4398e0f0b5415703d200a73
celia.gutierrez <zelia.cgr@gmail.com>
2024-11-13 21:51:54 -0500

    removiendo comentarios

--- desarrollo/ejercicio2.js
+++ desarrollo/ejercicio2.js
@@ -1,9 +1,9 @@
-// Seleccionamos el formulario, el campo de entrada de la nota y el párrafo de resultado
+
 const formulario = document.getElementById('formulario');
 const inputNota = document.getElementById('nota');
 const resultado = document.getElementById('resultado');
 
-// Función para determinar si la nota es aprobatoria o no
+
 function verificarNota(nota) {
     if (nota >= 11 && nota <= 20) {
         return '¡Felicidades! Aprobaste.';
@@ -14,10 +14,10 @@ function verificarNota(nota) {
     }
 }
 
-// Agregamos un evento al formulario para manejar la verificación de la nota al enviar
+
 formulario.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y recargue la página
-    const nota = parseInt(inputNota.value); // Convertimos el valor a número
-    const mensaje = verificarNota(nota); // Llamamos a la función para verificar la nota
-    resultado.textContent = mensaje; // Mostramos el mensaje en el párrafo resultado
+    event.preventDefault(); 
+    const nota = parseInt(inputNota.value); 
+    const mensaje = verificarNota(nota); 
+    resultado.textContent = mensaje; 
 });
--- desarrollo/ejercicio4.js
+++ desarrollo/ejercicio4.js
@@ -1,27 +1,27 @@
-// Función que recibe tres números y retorna el mayor de ellos
+
 function encontrarMayor(num1, num2, num3) {
     if (num1 >= num2 && num1 >= num3) {
-        return num1; // Si num1 es mayor o igual a los otros dos
+        return num1; 
     } else if (num2 >= num1 && num2 >= num3) {
-        return num2; // Si num2 es mayor o igual a los otros dos
+        return num2; 
     } else {
-        return num3; // Si num3 es mayor que los otros dos
+        return num3; 
     }
 }
 
-// Seleccionamos el formulario y los elementos de entrada de números
+
 const formulario = document.getElementById('formulario');
 const inputNumero1 = document.getElementById('numero1');
 const inputNumero2 = document.getElementById('numero2');
 const inputNumero3 = document.getElementById('numero3');
 const resultado = document.getElementById('resultado');
 
-// Agregamos un evento al formulario para manejar la verificación al enviar
+
 formulario.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+    event.preventDefault(); 
     const num1 = parseFloat(inputNumero1.value);
     const num2 = parseFloat(inputNumero2.value);
     const num3 = parseFloat(inputNumero3.value);
-    const mayor = encontrarMayor(num1, num2, num3); // Llamamos a la función con los tres números
-    resultado.textContent = `El número mayor es: ${mayor}`; // Mostramos el resultado en el párrafo
+    const mayor = encontrarMayor(num1, num2, num3); 
+    resultado.textContent = `El número mayor es: ${mayor}`; 
 });
